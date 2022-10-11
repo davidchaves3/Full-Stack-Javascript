0979116c4742260ab2585f44a16eb4bcd0d22625
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-10-11 15:36:22 -0300

    aula

---
+++ JAVASCRIPT-MODULO-1/aula-19/switch.js
@@ -0,0 +1,15 @@
+const resultado = prompt("Escolha uma alternativa de A a C")
+
+switch(resultado) {
+  case "a":
+    alert("O resultado é 'a' ")
+    break
+  case "b":
+    alert("O resultado é 'b' ")
+    break  
+  case "c":
+    alert("O resultado é 'c' ")
+    break  
+  default:
+    alert("Finalizado!")  
+}
