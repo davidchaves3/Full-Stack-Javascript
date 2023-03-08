33199a7c634e173e962abe51eef93f030a30348f
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2023-03-08 15:20:43 -0300

    atividade

--- JAVASCRIPT-MODULO-1/exercicios/ex0007/index.js
+++ JAVASCRIPT-MODULO-1/exercicios/ex0007/index.js
@@ -1,7 +1,34 @@
-do{
-  alert(
-    "Escolha uma Opção!\n" + 
-    "1)OPÇÃO UM\n" + 
-    "1)OPÇÃO UM\n" +
-  )
-}while(resp == "encerrar")
+let opcao = "5"
+
+do {
+    opcao = prompt(
+      "Seja Bem-Vindo(a)\n" + 
+      "\nEscolha uma das opções abaixo:" +
+      "\n1.Opção Um" +
+      "\n2.Opção Um" +
+      "\n3.Opção Um" +
+      "\n4.Opção Um" +
+      "\n5.Encerrar" 
+    )
+
+    switch (opcao) {
+      case "1":
+        alert("Você escolheu a opção 1")
+        break
+      case "2":
+        alert("Você escolheu a opção 2")
+        break
+      case "3":
+        alert("Você escolheu a opção 3")
+        break
+      case "4":
+        alert("Você escolheu a opção 4")
+        break
+      case "5":
+        alert("Você escolheu a opção 5")
+        break
+      default:
+        alert("Opção inválida")       
+    }
+
+} while (opcao !== "5")
